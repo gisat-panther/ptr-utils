7cfa67ee3ed91e0322e2df8753a9bf302d75b0d9
Vojtěch Dubrovský <v.dubr@hotmail.com>
2020-09-09 14:25:41 +0200

    Import utils funftionality from ptr-maps

---
+++ src/map/view.js
@@ -0,0 +1,242 @@
+import * as turf from '@turf/turf';
+import createCachedSelector from "re-reselect";
+import _ from "lodash";
+
+import constants from "./constants/view";
+
+/**
+ * Convert Web World Wind range to Panther boxRange
+ * @param range {WorldWind.Navigator.range}
+ * @param width {number} map width
+ * @param height {number} map height
+ * @return {number|*} Panther view box range
+ */
+function getBoxRangeFromWorldWindRange(range, width, height) {
+    if (width && width >= height) {
+        return range * height/width;
+    } else {
+        return range;
+    }
+}
+
+/**
+ * Convert Panther boxRange to Web World Wind range
+ * @param boxRange {number} Panther view boxRange
+ * @param width {number} map width
+ * @param height {number} map height
+ * @return {number|*} WorldWind navigator range
+ */
+function getWorldWindRangeFromBoxRange(boxRange, width, height) {
+    if (width && width >= height) {
+        return boxRange * width/height;
+    } else {
+        return boxRange;
+    }
+}
+
+/**
+ * @param level {number} zoom level
+ * @param width {number} map width
+ * @param height {number} map height
+ * @return {number} Panther box range
+ */
+function getBoxRangeFromZoomLevel(level, width, height) {
+    // remove 1 from box range to prevent rounding issues
+    return (Math.min(width, height) * getPixelSizeFromZoomLevel(level)) - 1;
+}
+
+/**
+ * @param boxRange {number} Panther view boxRange
+ * @param width {number} map width
+ * @param height {number} map height
+ * @return {number} Zoom level
+ */
+function getZoomLevelFromBoxRange(boxRange, width, height) {
+    // remove 1 from box range to prevent rounding issues
+    return getZoomLevelFromPixelSize((boxRange - 1)/Math.min(width, height));
+}
+
+/**
+ * @param level {number} zoom level
+ * @return {number} Size of 1 px in meters
+ */
+function getPixelSizeFromZoomLevel(level) {
+    return constants.pixelSizeInLevels[level];
+}
+
+/**
+ * Calculate zoom level from pixel size in meters. It returns next lower (or the lowest) level
+ * @param pxSize {number} pixel size in meters
+ * @return {number} Zoom level
+ */
+function getZoomLevelFromPixelSize(pxSize) {
+    const levels = constants.pixelSizeInLevels;
+
+    let level = 0;
+    while (pxSize <= levels[level + 1] && level < levels.length) {
+        level++;
+    }
+    return level;
+}
+
+/**
+ *
+ * @param bbox {{minLat: number, minLon: number, maxLat: number, maxLon: number}}
+ * @param latitude {boolean}
+ * @return {number}
+ */
+function getBoxRangeFromBoundingBox(bbox, latitude) {
+	const MIN_BOX_RANGE = 1000;
+	const RANGE_COEFF = 125000; //approximately one degree of longitude on equator in meters
+
+	// TODO naive for now
+	let latDiff = Math.abs(bbox.maxLat - bbox.minLat);
+	let lonDiff = Math.abs(bbox.maxLon - bbox.minLon);
+
+	let diff = Math.max(latDiff, lonDiff);
+	let boxRange = RANGE_COEFF*diff;
+	if (latitude) {
+		boxRange /= Math.cos(latitude * Math.PI/180)
+    }
+
+	return boxRange > MIN_BOX_RANGE ? boxRange : MIN_BOX_RANGE;
+}
+
+// TODO naive for now
+function getViewFromBoundingBox(bbox, reflectLatitude) {
+	if (_.isArray(bbox)) {
+		bbox = {
+			minLat: bbox[1],
+			minLon: bbox[0],
+			maxLat: bbox[3],
+			maxLon: bbox[2]
+		}
+	}
+
+	const center = {
+		lat: (bbox.maxLat + bbox.minLat)/2,
+		lon: (bbox.maxLon + bbox.minLon)/2
+	};
+
+	const boxRange = getBoxRangeFromBoundingBox(bbox, reflectLatitude ? center.lat : null);
+
+	return {
+		center,
+		boxRange
+	}
+}
+
+/**
+ * @param geometry {Object} geojson geometry
+ * @param reflectLatitude {boolean}
+ * @return {Object} view
+ */
+function getViewFromGeometry(geometry, reflectLatitude) {
+	let center = turf.center(geometry);
+	let bbox = turf.bbox(geometry);
+	let boxRange = getBoxRangeFromBoundingBox({
+		minLat: bbox[1],
+		minLon: bbox[0],
+		maxLat: bbox[3],
+		maxLon: bbox[2]
+	}, reflectLatitude ? center.geometry.coordinates[1] : null);
+
+	return {
+		center: {
+			lat: center.geometry.coordinates[1],
+			lon: center.geometry.coordinates[0]
+		},
+		boxRange
+	}
+}
+
+
+const ensureViewIntegrity = (view) => {
+	if (view) {
+		if (view.heading && view.heading > 360) {
+			view.heading = view.heading % 360;
+		}
+
+		if (view.heading && view.heading < 0) {
+			view.heading = 360 - (-view.heading % 360);
+		}
+
+		if (view.tilt && view.tilt < 0) {
+			view.tilt = 0;
+		}
+
+		if (view.tilt && view.tilt > 90) {
+			view.tilt = 90;
+		}
+
+		if (view.range && view.range < 0.01) {
+			view.range = 0.01;
+		}
+
+		if (view.center) {
+			if (view.center.lat) {
+				if (view.center.lat > 90) {
+					view.center.lat = 90;
+				} else if (view.center.lat < -90) {
+					view.center.lat = -90;
+				}
+			}
+
+			if (view.center.lon) {
+				if (view.center.lon > 360 || view.center.lon < -360) {
+					view.center.lon %= 360;
+				}
+
+				if (view.center.lon > 180) {
+					view.center.lon = -180 + (view.center.lon - 180);
+				} else if (view.center.lon < -180) {
+					view.center.lon = 180 + (view.center.lon + 180);
+				} else if (view.center.lon === -180) {
+					view.center.lon = 180;
+				}
+			}
+		}
+	}
+
+	return view;
+};
+
+const mergeViews = createCachedSelector(
+	[
+		(one) => one,
+		(one, two) => two,
+		(one, two, three) => three
+	],
+	(one, two, three) => {
+		three = three || {};
+		return {...one, ...two, ...three};
+	}
+)(
+	(one, two, three) => `${one}_${two}_${three || ""}`
+);
+
+export default {
+    ensureViewIntegrity,
+    mergeViews,
+    getBoxRangeFromWorldWindRange,
+    getBoxRangeFromZoomLevel,
+    getPixelSizeFromZoomLevel,
+    getViewFromBoundingBox,
+    getViewFromGeometry,
+    getWorldWindRangeFromBoxRange,
+    getZoomLevelFromBoxRange,
+    getZoomLevelFromPixelSize,
+}
+
+export {
+    ensureViewIntegrity,
+    mergeViews,
+    getBoxRangeFromWorldWindRange,
+    getBoxRangeFromZoomLevel,
+    getPixelSizeFromZoomLevel,
+    getViewFromBoundingBox,
+    getViewFromGeometry,
+    getWorldWindRangeFromBoxRange,
+    getZoomLevelFromBoxRange,
+    getZoomLevelFromPixelSize,
+}
---
+++ tests/map/view-test.js
@@ -0,0 +1,367 @@
+import {assert} from 'chai';
+import {cloneDeep} from 'lodash';
+
+import {
+    ensureViewIntegrity,
+    mergeViews,
+    getBoxRangeFromWorldWindRange,
+    getBoxRangeFromZoomLevel,
+    getPixelSizeFromZoomLevel,
+    getViewFromBoundingBox,
+    getViewFromGeometry,
+    getWorldWindRangeFromBoxRange,
+    getZoomLevelFromBoxRange,
+    getZoomLevelFromPixelSize,
+} from '../../src/map/view';
+
+const validView = {
+    heading: 0,
+    tilt: 0,
+    range: 5000,
+    center: {
+        lon: 14,
+        lat: 51,
+    }
+}
+
+const geometry = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [
+            [
+              [
+                16,51
+              ],
+              [
+                19,51
+              ],
+              [
+                19,52
+              ],
+              [
+                16,52
+              ],
+              [
+                16,51
+              ]
+            ]
+          ]
+        }
+      }
+    ]
+  }
+
+describe('ensureViewIntegrity', function () {
+
+	it('view is valid', function () {
+        assert.deepEqual(ensureViewIntegrity(cloneDeep(validView)), validView);
+    });
+
+	it('fix invalid heading 1', function () {
+        const invalidView = cloneDeep(validView);
+        invalidView.heading = -30;
+        
+        const validViewHeading = cloneDeep(validView);
+        validViewHeading.heading = 330;
+        assert.deepEqual(ensureViewIntegrity(invalidView), validViewHeading);
+    });
+
+	it('fix invalid heading 2', function () {
+        const invalidView = cloneDeep(validView);
+        invalidView.heading = 370;
+        
+        const validViewHeading = cloneDeep(validView);
+        validViewHeading.heading = 10;
+        assert.deepEqual(ensureViewIntegrity(invalidView), validViewHeading);
+    });
+
+	it('fix invalid tilt 1', function () {
+        const invalidView = cloneDeep(validView);
+        invalidView.tilt = 100;
+        
+        const validViewTilt = cloneDeep(validView);
+        validViewTilt.tilt = 90;
+        assert.deepEqual(ensureViewIntegrity(invalidView), validViewTilt);
+    });
+
+	it('fix invalid tilt 2', function () {
+        const invalidView = cloneDeep(validView);
+        invalidView.tilt = -50;
+        
+        const validViewTilt = cloneDeep(validView);
+        validViewTilt.tilt = 0;
+        assert.deepEqual(ensureViewIntegrity(invalidView), validViewTilt);
+    });
+
+	it('fix invalid range', function () {
+        const invalidView = cloneDeep(validView);
+        invalidView.range = -100;
+        
+        const validViewRange = cloneDeep(validView);
+        validViewRange.range = 0.01;
+        assert.deepEqual(ensureViewIntegrity(invalidView), validViewRange);
+    });
+
+	it('fix invalid center longitude', function () {
+        const invalidView = cloneDeep(validView);
+        invalidView.center.lon = 400;
+        
+        const validViewCenterLon = cloneDeep(validView);
+        validViewCenterLon.center.lon = 40;
+        assert.deepEqual(ensureViewIntegrity(invalidView), validViewCenterLon);
+    });
+
+	it('fix invalid center longitude', function () {
+        const invalidView = cloneDeep(validView);
+        invalidView.center.lon = 180;
+        
+        const validViewCenterLon = cloneDeep(validView);
+        validViewCenterLon.center.lon = 180;
+        assert.deepEqual(ensureViewIntegrity(invalidView), validViewCenterLon);
+    });
+
+	it('fix invalid center longitude', function () {
+        const invalidView = cloneDeep(validView);
+        invalidView.center.lon = 181;
+        
+        const validViewCenterLon = cloneDeep(validView);
+        validViewCenterLon.center.lon = -179;
+        assert.deepEqual(ensureViewIntegrity(invalidView), validViewCenterLon);
+    });
+
+	it('fix invalid center longitude', function () {
+        const invalidView = cloneDeep(validView);
+        invalidView.center.lon = -400;
+        
+        const validViewCenterLon = cloneDeep(validView);
+        validViewCenterLon.center.lon = -40;
+        assert.deepEqual(ensureViewIntegrity(invalidView), validViewCenterLon);
+    });
+
+	it('fix invalid center longitude', function () {
+        const invalidView = cloneDeep(validView);
+        invalidView.center.lon = -180;
+        
+        const validViewCenterLon = cloneDeep(validView);
+        validViewCenterLon.center.lon = 180;
+        assert.deepEqual(ensureViewIntegrity(invalidView), validViewCenterLon);
+    });
+
+	it('fix invalid center longitude', function () {
+        const invalidView = cloneDeep(validView);
+        invalidView.center.lon = -181;
+        
+        const validViewCenterLon = cloneDeep(validView);
+        validViewCenterLon.center.lon = 179;
+        assert.deepEqual(ensureViewIntegrity(invalidView), validViewCenterLon);
+    });
+});
+
+describe('mergeViews', function () {
+
+	it('merge two views', function () {
+        const viewOne = {
+            a:1
+        };
+
+        const viewTwo = {
+            b:2
+        }
+
+
+        assert.deepEqual(mergeViews(viewOne, viewTwo), {a:1,b:2});
+    });
+
+	it('returns cached result', function () {
+        const viewOne = {
+            a:1
+        };
+
+        const viewTwo = {
+            b:2
+        }
+
+        const mergeOne = mergeViews(viewOne, viewTwo);
+        const mergeTwo = mergeViews(viewOne, viewTwo);
+
+        assert.equal(mergeOne, mergeTwo);
+    });
+
+	it('merge three views', function () {
+        const viewOne = {
+            a:1
+        };
+
+        const viewTwo = {
+            b:2
+        }
+
+        const viewThree = {
+            c:3
+        }
+
+
+        assert.deepEqual(mergeViews(viewOne, viewTwo, viewThree), {a:1, b:2, c:3});
+    });
+});
+
+describe('getBoxRangeFromWorldWindRange', function () {
+
+	it('returns half range', function () {
+        const range = 10000;
+        const width = 600; //px
+        const height = 300; //px
+        const wwwRange = getBoxRangeFromWorldWindRange(range, width, height);
+
+        assert.equal(wwwRange, 5000);
+    });
+
+	it('returns same range', function () {
+        const range = 10000;
+        const width = 300; //px
+        const height = 600; //px
+        const wwwRange = getBoxRangeFromWorldWindRange(range, width, height);
+
+        assert.equal(wwwRange, 10000);
+    });
+
+});
+
+describe('getBoxRangeFromZoomLevel', function () {
+
+	it('returns box range', function () {
+        const width = 600; //px
+        const height = 300; //px
+        const boxRange = getBoxRangeFromZoomLevel(0, width, height);
+
+        assert.equal(boxRange, 30187175.77750529);
+    });
+
+	it('returns box range', function () {
+        const width = 300; //px
+        const height = 600; //px
+        const boxRange = getBoxRangeFromZoomLevel(0, width, height);
+
+        assert.equal(boxRange, 30187175.77750529);
+    });
+});
+
+describe('getPixelSizeFromZoomLevel', function () {
+
+	it('returns pixel size for zero level in default latitude', function () {
+        const pixelSize = getPixelSizeFromZoomLevel(0);
+        assert.equal(pixelSize, 100623.9225916843);
+    });
+
+	it('returns pixel size for 9-th level in default latitude', function () {
+        const pixelSize = getPixelSizeFromZoomLevel(9);
+        assert.equal(pixelSize, 196.53109881188323);
+    });
+});
+
+describe('getViewFromBoundingBox', function () {
+
+	it('returns bounding box', function () {
+        const BBox = getViewFromBoundingBox([10, 10, -10, -10], true);
+
+        assert.deepEqual(BBox.center, {lon:0, lat:0});
+        assert.deepEqual(BBox.boxRange, 2500000);
+    });
+
+	it('returns bounding box', function () {
+        const BBox = getViewFromBoundingBox([90, 90, -10, -10]);
+
+        assert.deepEqual(BBox.center, {lon:40, lat:40});
+        assert.deepEqual(BBox.boxRange, 12500000);
+    });
+
+	it('returns bounding box', function () {
+        const BBox = getViewFromBoundingBox([90, 90, -10, -10], true);
+
+        assert.deepEqual(BBox.center, {lon:40, lat:40});
+        assert.deepEqual(BBox.boxRange, 16317591.116653483);
+    });
+});
+
+describe('getViewFromGeometry', function () {
+
+	it('returns view centered on geometry', function () {
+        const view = getViewFromGeometry(geometry, true);
+
+        assert.deepEqual(view, {
+            "boxRange": 602395.4746276854,
+            "center": {
+            "lat": 51.5,
+            "lon": 17.5
+            }
+        })
+    });
+
+	it('returns view centered on geometry', function () {
+        const view = getViewFromGeometry(geometry);
+
+        assert.deepEqual(view, {
+            "boxRange": 375000,
+            "center": {
+            "lat": 51.5,
+            "lon": 17.5
+            }
+        })
+    });
+
+});
+
+describe('getWorldWindRangeFromBoxRange', function () {
+
+	it('returns worldwind range from boxRange', function () {
+
+        const boxRange = 10000;
+        const width = 600; //px
+        const height = 300; //px
+        const wwwRange = getWorldWindRangeFromBoxRange(boxRange, width, height);
+
+        assert.equal(wwwRange, 20000);
+    });
+
+	it('returns worldwind range from boxRange', function () {
+
+        const boxRange = 10000;
+        const width = 300; //px
+        const height = 600; //px
+        const wwwRange = getWorldWindRangeFromBoxRange(boxRange, width, height);
+
+        assert.equal(wwwRange, 10000);
+    });
+});
+
+describe('getZoomLevelFromBoxRange', function () {
+
+	it('returns worldwind range from boxRange', function () {
+        const boxRange = 1000;
+        const width = 300; //px
+        const height = 600; //px
+        const level = getZoomLevelFromBoxRange(boxRange, width, height);
+
+        assert.equal(level, 14);
+    });
+
+});
+
+describe('getZoomLevelFromPixelSize', function () {
+
+	it('Identify level by pixelsize', function () {
+        const level = getZoomLevelFromPixelSize(1000000);
+        assert.equal(level, 0);
+    });
+
+	it('Identify level by pixelsize', function () {
+        const level = getZoomLevelFromPixelSize(1);
+        assert.equal(level, 16);
+    });
+
+});
